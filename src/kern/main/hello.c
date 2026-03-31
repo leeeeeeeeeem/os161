f8ac53160d26b927d3bd2712ceccaf35a2e75730
Stefano Lemerle <stefano.lemerle@gmail.com>
2026-03-31 10:20:22 +0200

    Feat: Added project files

---
+++ src/kern/main/hello.c
@@ -0,0 +1,24 @@
+#include <types.h>
+#include <kern/errno.h>
+#include <kern/reboot.h>
+#include <kern/unistd.h>
+#include <lib.h>
+#include <spl.h>
+#include <clock.h>
+#include <thread.h>
+#include <proc.h>
+#include <current.h>
+#include <synch.h>
+#include <vm.h>
+#include <mainbus.h>
+#include <vfs.h>
+#include <device.h>
+#include <syscall.h>
+#include <test.h>
+#include <version.h>
+#include <hello.h>
+#include "autoconf.h"  // for pseudoconfig
+
+void hello(){
+	kprintf("Benvenuto nel kernel dello swag\n");
+}
